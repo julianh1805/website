b220df4f713093f2f9ed81c756564611ef902ec3
Julianh1805 <husson.j@yahoo.com>
2021-10-07 16:20:34 +0200

    etudes component

--- src/views/Formation.vue
+++ src/views/Formation.vue
@@ -1,10 +1,14 @@
 <template>
-  <p>FORMATION</p>
+  <Etudes></Etudes>
 </template>
 
 <script>
+import Etudes from '@/components/Etudes.vue'
 
 export default {
   name: 'Formation',
+  components:{
+    Etudes
+  }
 }
 </script>
--- src/views/Qualites.vue
+++ src/views/Qualites.vue
@@ -1,5 +1,4 @@
 <template>
-  <p>QUALITES</p>
 </template>
 
 <script>
