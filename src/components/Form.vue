8165a308ebd6288e3e10440ada2d496f4789c45b
Julianh1805 <husson.j@yahoo.com>
2021-11-16 21:54:08 +0100

    emailjs integration

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,19 +7,25 @@
           </a>
         </h2>
       <section id="content">
-        <form>
+        <form @submit.prevent="onSubmit" v-if="!loading && !send">
           <div class="form-group">
             <label for="name">Votre nom : </label>
-            <input type="text" name="name" id="name" placeholder="Juliano" required>
+            <input type="text" name="name" id="name" v-model="name" placeholder="Juliano" required v-bind:class="{'invalid': submitted && name === ''}">
+            <span class="invalid" v-if="submitted && name === ''">Vous êtes qui ?</span>
           </div>
           <div class="form-group">
             <label for="message">Votre message : </label>
-            <textarea  name="message" id="message" placeholder="Ciao, come stai ?" rows="4"></textarea>
+            <textarea  name="message" id="message" v-model="message" placeholder="Ciao, come stai ?" rows="4" v-bind:class="{'invalid': submitted && message === ''}"></textarea>
+            <span class="invalid" v-if="submitted && message === ''">Vous voulez quoi ?</span>
           </div>
           <div class="cta form-button">
-            <button class="draw-main trans-main send">Envoyer</button>
+            <button class="draw-main trans-main send" value="Send" type="submit">Envoyer</button>
           </div>
         </form>
+        <LoadingSpinner v-if="loading"></LoadingSpinner>
+        <div class="centered" v-if="!loading && send">
+          <p>Cool ! Votre message a bien été envoyé. Si je mets trop de temps à repondre, vous pouvez me trouver tous les soirs au Bar Bière Numéro 8</p>
+        </div>
         <div id="cordonnees">
           <hr class="lg-separator" v-if="windowWidth < 768">
           <div class="item" v-for="item in items">
@@ -35,17 +41,48 @@
 </template>
 
 <script>
+import emailjs from 'emailjs-com';
+import LoadingSpinner from "./LoadingSpinner";
+
 export default {
   name: 'Form',
-
+  components: {LoadingSpinner},
   data() {
     return {
+      name: '',
+      message: '',
+      submitted: false,
+      loading: false,
+      send: false,
       windowWidth: window.innerWidth,
       items: [
         {class: 'fa-map-marker-alt', text: 'Nantes (Bretagne), France'},
         {class: 'fa-phone-alt', text: '06 85 48 79 66'},
         {class: 'fa-envelope', text: 'husson.j@yahoo.com'},
       ]
+    }
+  },
+  methods: {
+    onSubmit(e){
+      this.submitted = true;
+      if(this.name && this.message){
+        try {
+          this.loading = true;
+          emailjs.sendForm(process.env.VUE_APP_EMAILJS_SERVICE_ID, process.env.VUE_APP_EMAILJS_TEMPLATE_ID, e.target,
+              process.env.VUE_APP_EMAILJS_USER_ID, {
+                name: this.name,
+                message: this.message
+              }).then(res => {
+                this.name = '';
+                this.email = '';
+                this.send = true;
+                this.loading = false
+          })
+        } catch(error) {
+          this.loading = false;
+          console.log({error})
+        }
+      }
     }
   },
   beforeMount(){
@@ -78,10 +115,16 @@ export default {
   }
   #cordonnees{
     display: flex;
-    align-items: center;
     .item{
-      padding-bottom: 18px;
-      margin-bottom: 18px;
+      padding-bottom: 36px;
+      width: 70%;
+      &:not(:last-child){
+        p{
+          padding-bottom: 20px;
+          border-bottom: 1px solid #86a1cd;
+          margin: auto;
+          }
+        }
       span{
         color: $main-color;
         font-size: 28px;
@@ -97,12 +140,7 @@ export default {
   #cordonnees{
     flex-direction: column;
     justify-content: space-between;
-    .item{
-      width: 40%;
-      &:not(:last-child){
-        border-bottom: 1px solid #86a1cd;
-      }
-    }
+    align-items: center;
   }
 }
 
@@ -117,14 +155,12 @@ export default {
 
 @media (min-width: 768px) {
   #content {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 3fr 2fr;
     justify-content: space-between;
   }
   #cordonnees{
     flex-direction: column;
-    .item &:not(:last-child){
-      border-bottom: 1px solid #86a1cd;
-    }
+    align-items: end;
   }
 }
 </style>
